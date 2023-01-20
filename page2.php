af28382a19a6ad0ca3f5c3855f729dd6d329bafb
Selli <83934061+Marcelina25@users.noreply.github.com>
2023-01-20 07:57:10 +0700

    Add files via upload

---
+++ page2.php
@@ -0,0 +1,35 @@
+<CENTER>
+<table border="1px" width="70%" cellpadding="5" cellspacing="0">
+<tr>
+	<th>No :</th>
+	<th>Nama :</th>
+	<th>Jabatan</th>
+	<th>NO Hp:</th>
+</tr>
+<tr>
+	<td>1.</td>
+	<td>MARCELINA NOVI ZARTI</td>
+	<td>DIREKTUR UTAMA</td>
+	<td>085783698716</td>
+</tr>
+<tr>
+	<td>2.</td>
+	<td>KURNIA PADILAH</td>
+	<td>WAKIL DIREKTUR</td>
+	<td>083168724897</td>
+</tr>
+<tr>
+	<td>3.</td>
+	<td>FITRI LESTARI</td>
+	<td>SEKRETARIS</td>
+	<td>081272628614</td>
+</tr>
+<tr>
+	<td>3.</td>
+	<td>ADINDA TRISISTA</td>
+	<td>BENDAHARA</td>
+	<td>085210792281</td>
+</tr>
+
+ </table>
+</CENTER>
